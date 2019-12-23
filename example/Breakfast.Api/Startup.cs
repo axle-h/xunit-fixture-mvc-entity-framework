376ac5ce32887e399ecb4ee7d098bb22c6983edb
Alex Haslehurst <alex.haslehurst@gmail.com>
2019-12-23 14:40:17 +0000

    ASP.NET 3.1 support

--- example/Breakfast.Api/Startup.cs
+++ example/Breakfast.Api/Startup.cs
@@ -5,8 +5,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using Swashbuckle.AspNetCore.Swagger;
 
 namespace Breakfast.Api
 {
@@ -21,16 +22,16 @@ namespace Breakfast.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers();
             services.AddSwaggerGen(c =>
                                    {
-                                       c.SwaggerDoc("v1", new Info { Title = "Breakfast API", Version = "v1" });
+                                       c.SwaggerDoc("v1", new OpenApiInfo { Title = "Breakfast API", Version = "v1" });
                                    });
 
             services.AddDbContext<BreakfastContext>(o => o.UseMySql(_mysqlConnectionString, mo => mo.ServerVersion(new Version(5, 7, 21), ServerType.MySql)));
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -40,8 +41,8 @@ namespace Breakfast.Api
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Breakfast API V1"));
 
-
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
--- src/Xunit.Fixture.Mvc.EntityFramework/Extensions/AssertionExtensions.cs
+++ src/Xunit.Fixture.Mvc.EntityFramework/Extensions/AssertionExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentAssertions.Equivalency;
